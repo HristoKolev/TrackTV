e4fe6db874f47f23c3c8d63c97381a0a24d96344
Hristo Kolev <hristo.a.kolev@gmail.com>
2015-10-03 16:23:58 +0300

    added webclient project

---
+++ src/TrackTV.WebClient/Startup.cs
@@ -0,0 +1,19 @@
+﻿namespace TrackTV.WebClient
+{
+    using Microsoft.AspNet.Builder;
+    using Microsoft.Framework.DependencyInjection;
+
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseFileServer();
+
+            app.Run(async context => { context.Response.Redirect("/"); });
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+    }
+}
