f6df88d48135d6da7926c04d2190ff026de60ec2
Hristo Kolev <hristo.a.kolev@gmail.com>
2015-11-09 12:00:47 +0200

    listResources tested

--- src/TrackTV.WebClient/modules/list-resources.js
+++ src/TrackTV.WebClient/modules/list-resources.js
@@ -3,18 +3,54 @@
 var path = require('path'),
     linuxStylePath = require('./linuxStylePath');
 
-module.exports = function (resourceArray, formatter, baseDir) {
+function isFunction(func) {
 
-    baseDir = baseDir || '';
+    var getType = {};
+
+    return func && getType.toString.call(func) === '[object Function]';
+}
+
+function list(resources, formatter, basePath) {
+
+    if (!resources) {
+
+        throw new Error('The resources are invalid.');
+    }
+
+    if (!Array.isArray(resources)) {
+
+        throw new Error('The resources object is not an array.');
+    }
+
+    if (!formatter) {
+
+        throw new Error('The formatter is invalid.');
+    }
+
+    if (!isFunction(formatter)) {
+
+        throw new Error('The formatter is not a function.');
+    }
+
+    basePath = basePath || '';
+
+    if (typeof basePath !== 'string') {
+
+        throw new Error('The base path is not a string.');
+    }
 
     var lines = [];
 
-    for (var i = 0; i < resourceArray.length; i += 1) {
+    for (var i = 0; i < resources.length; i += 1) {
 
-        var resourcePath = linuxStylePath(path.join('./', baseDir, resourceArray[i]));
+        var resourcePath = linuxStylePath(path.join('./', basePath, resources[i]));
 
         lines.push(formatter(resourcePath));
     }
 
-    return lines.join('\n');
-};
+    var separator = '\n';
+
+    return lines.join(separator);
+}
+
+module.exports = list;
---
+++ src/TrackTV.WebClient/test/listResources.test.js
@@ -0,0 +1,206 @@
+"use strict";
+
+var expect = require('chai').expect,
+    sinon = require('sinon');
+
+var assertCompositionMultitest = require('../testing/assertComposition').multitest;
+
+var listResources = require('../modules/list-resources');
+
+describe('#listResources()', function () {
+
+    describe('module exports', function () {
+
+        assertCompositionMultitest.function(listResources, 'listResources');
+    });
+
+    describe('#list()', function () {
+
+        var defaultResources = ['path1/file', 'path2/file', 'path3/file'];
+
+        var formatters = {
+
+            defaultFormatter: function () {
+
+                return '';
+            },
+
+            echoFormatter: function (arg) {
+
+                return arg;
+            }
+        };
+
+        var defaultFormatterSpy = sinon.spy(formatters, "defaultFormatter");
+
+        var echoFormatterSpy = sinon.spy(formatters, "echoFormatter");
+
+        function resetMocks() {
+
+            defaultFormatterSpy.reset();
+            echoFormatterSpy.reset();
+        }
+
+        var defaultBasePath = 'path';
+
+        beforeEach(function () {
+
+            resetMocks();
+        });
+
+        it('should throw if the resources argument are falsy', function () {
+
+            expect(function () {
+
+                listResources(null, formatters.defaultFormatter, defaultBasePath);
+
+            }).to.throw(/resources are invalid/);
+        });
+
+        it('should throw if the resources argument is not an array', function () {
+
+            var resources = 'resources';
+
+            expect(function () {
+
+                listResources(resources, formatters.defaultFormatter, defaultBasePath);
+
+            }).to.throw(/resources object is not an array/);
+        });
+
+        it('should throw if the formatter is falsy', function () {
+
+            expect(function () {
+
+                listResources(defaultResources, null, defaultBasePath);
+
+            }).to.throw(/formatter is invalid/);
+
+        });
+
+        it('should throw if the formatter is not a function', function () {
+
+            var formatter = 'formatter';
+
+            expect(function () {
+
+                listResources(defaultResources, formatter, defaultBasePath);
+
+            }).to.throw(/formatter is not a function/);
+
+        });
+
+        it('should throw if the basePath is provided but is not a string', function () {
+            var basePath = 1;
+
+            expect(function () {
+
+                listResources(defaultResources, formatters.defaultFormatter, basePath);
+
+            }).to.throw(/base path is not a string/);
+        });
+
+        it('should call the formatter for every element in the resources array', function () {
+
+            var resources = ['path1', 'path2', 'path3'];
+
+            listResources(resources, formatters.defaultFormatter, defaultBasePath);
+
+            expect(defaultFormatterSpy.callCount).to.equal(resources.length);
+        });
+
+        it('should format the paths', function () {
+
+            var basePath = 'base';
+
+            listResources(defaultResources, formatters.echoFormatter, basePath);
+
+            var results = [];
+
+            for (var i = 0; i < defaultResources.length; i += 1) {
+
+                results.push(echoFormatterSpy.args[i][0]);
+            }
+
+            var expectedPaths = [
+                'base/path1/file',
+                'base/path2/file',
+                'base/path3/file'
+            ];
+
+            expect(results).to.deep.equal(expectedPaths);
+        });
+
+        it('should not add base path if the not specified', function () {
+
+            listResources(defaultResources, formatters.echoFormatter);
+
+            var results = [];
+
+            for (var i = 0; i < defaultResources.length; i += 1) {
+
+                results.push(echoFormatterSpy.args[i][0]);
+            }
+
+            var expectedPaths = [
+                'path1/file',
+                'path2/file',
+                'path3/file'
+            ];
+
+            expect(results).to.deep.equal(expectedPaths);
+        });
+
+        it('should format the paths in linux style /', function () {
+
+            var resources = [
+                'path1\\file',
+                'path2\\file',
+                'path3\\file'
+            ];
+
+            listResources(resources, formatters.echoFormatter);
+
+            var results = [];
+
+            for (var i = 0; i < defaultResources.length; i += 1) {
+
+                results.push(echoFormatterSpy.args[i][0]);
+            }
+
+            var expectedPaths = [
+                'path1/file',
+                'path2/file',
+                'path3/file'
+            ];
+
+            expect(results).to.deep.equal(expectedPaths);
+        });
+
+        it('should remove the path separator from the start of the path', function () {
+
+            var resources = [
+                '/path1/file',
+                '/path2/file',
+                '/path3/file'
+            ];
+
+            listResources(resources, formatters.echoFormatter);
+
+            var results = [];
+
+            for (var i = 0; i < defaultResources.length; i += 1) {
+
+                results.push(echoFormatterSpy.args[i][0]);
+            }
+
+            var expectedPaths = [
+                'path1/file',
+                'path2/file',
+                'path3/file'
+            ];
+
+            expect(results).to.deep.equal(expectedPaths);
+        });
+    });
+});
