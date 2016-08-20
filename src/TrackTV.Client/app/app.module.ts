d440ebd92307ad47a4e24864e32507f854515311
Hristo Kolev <hristo.a.kolev@gmail.com>
2016-08-20 22:48:45 +0300

    fiexed spacing in app.module

--- src/TrackTV.Client/app/app.module.ts
+++ src/TrackTV.Client/app/app.module.ts
@@ -1,13 +1,13 @@
-import {NgModule}      from '@angular/core';
+import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {RouterModule}  from '@angular/router';
-import {HttpModule}  from '@angular/http';
+import {RouterModule} from '@angular/router';
+import {HttpModule} from '@angular/http';
 import {FormsModule, ReactiveFormsModule} from  '@angular/forms';
 
-import {AppComponent}  from './app.component';
+import {AppComponent} from './app.component';
 
 import {typeBindings} from './app.providers';
-import {routes} from  './app.routes';
+import {routes} from './app.routes';
 import {appDeclarations} from  './app.declarations';
 
 @NgModule({
