748249443d4a617e4debb73d9de4930b0240e127
Hristo Kolev <hristo.a.kolev@gmail.com>
2016-08-20 22:44:46 +0300

    added my-shows component

---
+++ src/TrackTV.Client/app/my-shows/my-shows.component.ts
@@ -0,0 +1,10 @@
+import {Component} from  '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-shows-component',
+    templateUrl: 'my-shows.component.html',
+})
+export class MyShowsComponent {
+
+}
