0d366a4cef8e7a4793bdd6f1ef9915c1b051eac2
Hristo Kolev <hristo.a.kolev@gmail.com>
2016-09-06 00:38:58 +0300

    non functional refactoring

--- src/TrackTV.Client/app/config/systemjs.config.ts
+++ src/TrackTV.Client/app/config/systemjs.config.ts
@@ -2,11 +2,23 @@ System.config({
     map: {
         '@angular': 'node_modules/@angular',
     },
+    meta: {
+        '@angular/*': {'format': 'cjs'}
+    },
+    paths: {
+        'node_modules/@angular/*': 'node_modules/@angular/*/bundles',
+
+        'jquery': './node_modules/jquery/dist/jquery.js',
+        'toastr': './node_modules/toastr/toastr.js',
+        'moment': './node_modules/moment/moment.js',
+        'underscore.string': './node_modules/underscore.string/dist/underscore.string.js',
+    },
     packages: {
         'app': {
             main: 'main',
             defaultExtension: 'js'
         },
+
         '@angular/core': {main: 'core.umd.min.js'},
         '@angular/common': {main: 'common.umd.min.js'},
         '@angular/compiler': {main: 'compiler.umd.min.js'},
@@ -14,20 +26,9 @@ System.config({
         '@angular/platform-browser-dynamic': {main: 'platform-browser-dynamic.umd.min.js'},
 
         '@angular/router': {main: 'router.umd.min.js'},
-
-        '@angular/forms': {main: 'forms.umd.min.js'},
         '@angular/http': {main: 'http.umd.min.js'},
-    },
-    meta: {
-        '@angular/*': {'format': 'cjs'}
-    },
-    paths: {
-        jquery: './node_modules/jquery/dist/jquery.js',
-        toastr: './node_modules/toastr/toastr.js',
-        'node_modules/@angular/*': 'node_modules/@angular/*/bundles',
-        'underscore.string': './node_modules/underscore.string/dist/underscore.string.js',
-        'moment': './node_modules/moment/moment.js',
-    },
+        '@angular/forms': {main: 'forms.umd.min.js'}
+    }
 });
 
 System.import('app/main')
