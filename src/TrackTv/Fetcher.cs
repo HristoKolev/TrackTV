251837b651f9c998500881a0de6d719ca715c2a2
Hristo Kolev <hristo.a.kolev@gmail.com>
2016-10-22 14:52:40 +0300

    Created fetcher class and json file for the credentials, added the reset client as a dependency updated the test assembly's core library

---
+++ src/TrackTv/Fetcher.cs
@@ -0,0 +1,26 @@
+﻿namespace TrackTv
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using TvDbSharper;
+
+    public class Fetcher
+    {
+        // ReSharper disable once StyleCop.SA1305
+        public Fetcher(TrackTvDbContext context, ITvDbClient tvDbClient)
+        {
+            this.Context = context;
+            this.TvDbClient = tvDbClient;
+        }
+
+        private TrackTvDbContext Context { get; }
+
+        private ITvDbClient TvDbClient { get; }
+
+        public async Task AddShow(string imdbId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/TrackTv/Program.cs
+++ src/TrackTv/Program.cs
@@ -1,9 +1,14 @@
 ﻿namespace TrackTv
 {
-    using System;
+    using System.IO;
     using System.Threading.Tasks;
 
     using Microsoft.EntityFrameworkCore;
+
+    using Newtonsoft.Json;
+
+    using TvDbSharper;
+    using TvDbSharper.Clients.Authentication.Json;
 
     public class Program
     {
@@ -11,16 +16,34 @@
 
         public async Task MainAsync(string[] args)
         {
-            var builder = new DbContextOptionsBuilder();
-
-            builder.UseSqlServer(@"Server=.;Database=TrackTvDb;Trusted_Connection=True;MultipleActiveResultSets=True;");
-
-            using (var db = new TrackTvDbContext(builder.Options))
+            using (var context = await CreateContext())
             {
-                await db.Database.MigrateAsync();
+                var data = ReadConfig<AuthenticationData>("thetvdb.json");
 
-                Console.WriteLine(await db.Shows.AnyAsync());
+                // ReSharper disable once StyleCop.SA1305
+                var tvDbClient = new TvDbClient();
+
+                await tvDbClient.Authentication.AuthenticateAsync(data);
+
+                var fetcher = new Fetcher(context, tvDbClient);
+
+                await fetcher.AddShow("tt4574334");
             }
         }
+
+        private static async Task<TrackTvDbContext> CreateContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder();
+
+            optionsBuilder.UseSqlServer(@"Server=.;Database=TrackTvDb;Trusted_Connection=True;MultipleActiveResultSets=True;");
+
+            var context = new TrackTvDbContext(optionsBuilder.Options);
+
+            await context.Database.MigrateAsync();
+
+            return context;
+        }
+
+        private static T ReadConfig<T>(string path) => JsonConvert.DeserializeObject<T>(File.ReadAllText(path));
     }
 }
