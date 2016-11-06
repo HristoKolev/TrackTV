25e266d89aaedd3b1fe61962b116015b25ba12ab
Hristo Kolev <hristo.a.kolev@gmail.com>
2016-11-06 21:33:21 +0200

    fixed project name casing

--- test/TrackTv.DataRetrieval.Tests/Properties/AssemblyInfo.cs
+++ test/TrackTv.DataRetrieval.Tests/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TrackTV.DataRetrieval.Tests")]
+[assembly: AssemblyProduct("TrackTv.DataRetrieval.Tests")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
