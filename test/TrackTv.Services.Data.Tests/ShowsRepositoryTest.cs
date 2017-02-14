bd5283fb7353a8c51679ab6d153faf5acb93d5d2
HristoKolev <hristo.a.kolev@gmail.com>
2017-02-15 00:09:49 +0200

    added test for the shows repository

---
+++ test/TrackTv.Services.Data.Tests/ShowsRepositoryTest.cs
@@ -0,0 +1,436 @@
+﻿namespace TrackTv.Services.Data.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using TrackTv.Models;
+    using TrackTv.Models.Joint;
+    using TrackTv.Services.Data.Exceptions;
+
+    using Xunit;
+
+    public class ShowsRepositoryTest : BaseRepositoryTest
+    {
+        [Fact]
+
+        // ReSharper disable once InconsistentNaming
+        public async Task CountAllAsync_returns_correct_count()
+        {
+            using (var context = CreateContext())
+            {
+                var list = new List<Show>();
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    var show = new Show
+                    {
+                        Id = i
+                    };
+
+                    list.Add(show);
+                }
+
+                context.Shows.AddRange(list);
+                await context.SaveChangesAsync();
+
+                var repository = new ShowsRepository(context);
+
+                Assert.Equal(10, await repository.CountAllAsync());
+            }
+        }
+
+        [Fact]
+
+        // ReSharper disable once InconsistentNaming
+        public async Task CountAllResultsAsync_returns_correct_count()
+        {
+            using (var context = CreateContext())
+            {
+                var list = new List<Show>();
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    var show = new Show
+                    {
+                        Id = i,
+                        Name = i % 2 == 0 ? "new show " + i : "old show " + i
+                    };
+
+                    list.Add(show);
+                }
+
+                context.Shows.AddRange(list);
+                await context.SaveChangesAsync();
+
+                var repository = new ShowsRepository(context);
+
+                Assert.Equal(5, await repository.CountAllResultsAsync("new show"));
+            }
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("  ")]
+        [InlineData("\t")]
+
+        // ReSharper disable once InconsistentNaming
+        public async Task CountAllResultsAsync_throws_if_query_is_invalid(string query)
+        {
+            using (var context = CreateContext())
+            {
+                var list = new List<Show>();
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    var show = new Show
+                    {
+                        Id = i,
+                        Name = i % 2 == 0 ? "new show " + i : "old show " + i
+                    };
+
+                    list.Add(show);
+                }
+
+                context.Shows.AddRange(list);
+                await context.SaveChangesAsync();
+
+                var repository = new ShowsRepository(context);
+
+                await Assert.ThrowsAsync<InvalidQueryException>(async () => await repository.CountAllResultsAsync(query));
+            }
+        }
+
+        [Fact]
+
+        // ReSharper disable once InconsistentNaming
+        public async Task CountByGenreAsync_returns_correct_count()
+        {
+            using (var context = CreateContext())
+            {
+                var list = new List<Show>();
+
+                var actionGenre = new Genre("action");
+                var dramaGenre = new Genre("drama");
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    var show = new Show
+                    {
+                        Id = i
+                    };
+
+                    show.ShowsGenres.Add(new ShowsGenres
+                    {
+                        Genre = i % 2 == 0 ? actionGenre : dramaGenre
+                    });
+
+                    list.Add(show);
+                }
+
+                context.Shows.AddRange(list);
+                await context.SaveChangesAsync();
+
+                var repository = new ShowsRepository(context);
+
+                Assert.Equal(5, await repository.CountByGenreAsync("action"));
+            }
+        }
+
+        [Fact]
+
+        // ReSharper disable once InconsistentNaming
+        public async Task CountSubscribersAsync_returns_correct_count()
+        {
+            using (var context = CreateContext())
+            {
+                var list = new List<Show>();
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    var show = new Show
+                    {
+                        Id = i
+                    };
+
+                    for (int j = 1; j <= 5; j++)
+                    {
+                        show.ShowsUsers.Add(new ShowsUsers
+                        {
+                            User = new User
+                            {
+                                Id = j
+                            }
+                        });
+                    }
+
+                    list.Add(show);
+                }
+
+                context.Shows.AddRange(list);
+                await context.SaveChangesAsync();
+
+                var repository = new ShowsRepository(context);
+
+                int[] showIds = {
+                    1,
+                    2,
+                    3
+                };
+
+                var summaries = await repository.CountSubscribersAsync(showIds);
+
+                foreach (int showId in showIds)
+                {
+                    var summary = summaries.Single(s => s.ShowId == showId);
+
+                    Assert.Equal(5, summary.SubscriberCount);
+                }
+            }
+        }
+
+        [Fact]
+
+        // ReSharper disable once InconsistentNaming
+        public async Task GetShowsByUserIdAsync_returns_correct_count()
+        {
+            using (var context = CreateContext())
+            {
+                var list = new List<Show>();
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    var show = new Show
+                    {
+                        Id = i
+                    };
+
+                    for (int j = 1; j <= 5; j++)
+                    {
+                        show.ShowsUsers.Add(new ShowsUsers
+                        {
+                            User = new User
+                            {
+                                Id = j
+                            }
+                        });
+                    }
+
+                    list.Add(show);
+                }
+
+                context.Shows.AddRange(list);
+                await context.SaveChangesAsync();
+
+                var repository = new ShowsRepository(context);
+
+                var shows = await repository.GetShowsByUserIdAsync(1);
+
+                Assert.Equal(10, shows.Length);
+            }
+        }
+
+        [Fact]
+
+        // ReSharper disable once InconsistentNaming
+        public async Task GetShowWithNetworkByIdAsync_returns_show_with_the_network()
+        {
+            using (var context = CreateContext())
+            {
+                var list = new List<Show>();
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    var show = new Show
+                    {
+                        Id = i,
+                        Network = new Network
+                        {
+                            Id = i
+                        }
+                    };
+
+                    list.Add(show);
+                }
+
+                context.Shows.AddRange(list);
+                await context.SaveChangesAsync();
+
+                var repository = new ShowsRepository(context);
+
+                var firstShow = await repository.GetShowWithNetworkByIdAsync(1);
+
+                Assert.Equal(1, firstShow.Id);
+                Assert.NotNull(firstShow.Network);
+            }
+        }
+
+        [Fact]
+
+        // ReSharper disable once InconsistentNaming
+        public async Task GetTopAsync_returns_the_correct_result()
+        {
+            using (var context = CreateContext())
+            {
+                var list = new List<Show>();
+
+                int userCount = 10;
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    var show = new Show
+                    {
+                        Id = i
+                    };
+
+                    for (int j = userCount - 1; j >= 0; j--)
+                    {
+                        show.ShowsUsers.Add(new ShowsUsers
+                        {
+                            User = new User()
+                        });
+                    }
+
+                    userCount -= 1;
+
+                    list.Add(show);
+                }
+
+                context.Shows.AddRange(list);
+                await context.SaveChangesAsync();
+
+                var repository = new ShowsRepository(context);
+
+                var shows = await repository.GetTopAsync(1, 5);
+
+                Assert.True(shows.Select(x => x.Id).SequenceEqual(Enumerable.Range(1, 5)));
+            }
+        }
+
+        [Fact]
+
+        // ReSharper disable once InconsistentNaming
+        public async Task GetTopByGenreAsync_returns_the_correct_result()
+        {
+            using (var context = CreateContext())
+            {
+                var list = new List<Show>();
+
+                int userCount = 10;
+
+                var actionGenre = new Genre("action");
+                var dramaGenre = new Genre("drama");
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    var show = new Show
+                    {
+                        Id = i
+                    };
+
+                    show.ShowsGenres.Add(new ShowsGenres
+                    {
+                        Genre = i % 2 == 0 ? actionGenre : dramaGenre
+                    });
+
+                    for (int j = userCount - 1; j >= 0; j--)
+                    {
+                        show.ShowsUsers.Add(new ShowsUsers
+                        {
+                            User = new User()
+                        });
+                    }
+
+                    userCount -= 1;
+
+                    list.Add(show);
+                }
+
+                context.Shows.AddRange(list);
+                await context.SaveChangesAsync();
+
+                var repository = new ShowsRepository(context);
+
+                var shows = await repository.GetTopByGenreAsync(dramaGenre.Name, 1, 5);
+
+                int[] expectedIds = {
+                    1,
+                    3,
+                    5,
+                    7,
+                    9
+                };
+
+                Assert.True(shows.Select(x => x.Id).SequenceEqual(expectedIds));
+            }
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("  ")]
+        [InlineData("\t")]
+
+        // ReSharper disable once InconsistentNaming
+        public async Task SearchTopAsync_throws_if_the_query_is_invalid(string query)
+        {
+            var repository = new ShowsRepository(null);
+
+            await Assert.ThrowsAsync<InvalidQueryException>(async () => await repository.SearchTopAsync(query, 1, 5));
+        }
+
+        [Fact]
+
+        // ReSharper disable once InconsistentNaming
+        public async Task SearchTopAsync_returns_correct_shows()
+        {
+            using (var context = CreateContext())
+            {
+                var list = new List<Show>();
+
+                int userCount = 10;
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    var show = new Show
+                    {
+                        Id = i,
+                        Name = i % 2 == 0 ? "new show " + i : "old show " + i
+                    };
+
+                    for (int j = userCount - 1; j >= 0; j--)
+                    {
+                        show.ShowsUsers.Add(new ShowsUsers
+                        {
+                            User = new User()
+                        });
+                    }
+
+                    userCount -= 1;
+
+                    list.Add(show);
+                }
+
+                context.Shows.AddRange(list);
+                await context.SaveChangesAsync();
+
+                var repository = new ShowsRepository(context);
+
+                var shows = await repository.SearchTopAsync("old show", 1, 5);
+
+                int[] ids = {
+                    1,
+                    3,
+                    5,
+                    7,
+                    9
+                };
+
+                Assert.True(shows.Select(x => x.Id).SequenceEqual(ids));
+            }
+        }
+    }
+}
