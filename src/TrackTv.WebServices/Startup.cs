fb675b9a6c5028291da8836bca39e35ed32f79ae
HristoKolev <hristo.a.kolev@gmail.com>
2017-03-03 17:07:01 +0200

    removed unused code

--- src/TrackTv.WebServices/Startup.cs
+++ src/TrackTv.WebServices/Startup.cs
@@ -1,7 +1,5 @@
 ﻿namespace TrackTv.WebServices
 {
-    using System.Threading.Tasks;
-
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Identity;
@@ -37,7 +35,11 @@
         public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public async void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, RoleManager<IdentityRole> roleManager)
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment env,
+            ILoggerFactory loggerFactory,
+            RoleManager<IdentityRole> roleManager)
         {
             loggerFactory.AddConsole(this.Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
@@ -56,7 +58,6 @@
             app.UseStaticFiles();
 
             app.UseIdentity();
-            await this.InitializeRoles(roleManager);
 
             // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
             app.UseMvc(routes => { routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}"); });
@@ -78,23 +79,6 @@
             // Add application services.
             services.AddTransient<IEmailSender, AuthMessageSender>();
             services.AddTransient<ISmsSender, AuthMessageSender>();
-        }
-
-        // Initialize some test roles. In the real world, these would be setup explicitly by a role manager
-        private string[] roles = { "User", "Admin" };
-
-        private async Task InitializeRoles(RoleManager<IdentityRole> roleManager)
-        {
-            foreach (var role in roles)
-            {
-                if (!await roleManager.RoleExistsAsync(role))
-                {
-                    var newRole = new IdentityRole(role);
-                    await roleManager.CreateAsync(newRole);
-                    // In the real world, there might be claims associated with roles
-                    // _roleManager.AddClaimAsync(newRole, new )
-                }
-            }
         }
     }
 }
