0bc4088ef43fa3be2000dd2ba97cda0d214a2b8e
HristoKolev <hristo.a.kolev@gmail.com>
2017-03-03 11:12:56 +0200

    Added web project

---
+++ src/TrackTv.WebServices/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿namespace TrackTv.WebServices.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : Controller
+    {
+        public IActionResult About()
+        {
+            this.ViewData["Message"] = "Your application description page.";
+
+            return this.View();
+        }
+
+        public IActionResult Contact()
+        {
+            this.ViewData["Message"] = "Your contact page.";
+
+            return this.View();
+        }
+
+        public IActionResult Error()
+        {
+            return this.View();
+        }
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
