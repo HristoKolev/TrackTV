9c867df19141e27ccfc5c8412e3fffd5f91544e5
HristoKolev <hristo.a.kolev@gmail.com>
2017-03-30 00:11:10 +0300

    deleted unused files

--- src/TrackTv.Services.Data/Models/SubscriberSummary.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TrackTv.Services.Data.Models
-{
-    public class SubscriberSummary
-    {
-        public int ShowId { get; set; }
-
-        public int SubscriberCount { get; set; }
-    }
-}
