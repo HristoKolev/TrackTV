ba67313199b28c5b02679524cb18a985d01f65f3
HristoKolev <hristo.a.kolev@gmail.com>
2018-10-19 21:11:36 +0300

    orm: paging

--- src/TrackTv.Data/QueryablePaging.cs
+++ src/TrackTv.Data/QueryablePaging.cs
@@ -3,15 +3,12 @@
     using System;
     using System.Linq;
 
-    public static class QuerablePaging
+    public static class QueryablePaging
     {
-        private const int MaxPageSize = 50;
-
         public static IQueryable<T> Page<T>(this IQueryable<T> queryable, int page, int pageSize)
         {
             page = Math.Max(page, 1);
             pageSize = Math.Max(pageSize, 1);
-            pageSize = Math.Min(pageSize, MaxPageSize);
 
             return queryable.Skip((page - 1) * pageSize).Take(pageSize);
         }
