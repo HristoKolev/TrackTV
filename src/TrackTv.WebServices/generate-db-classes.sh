bdeb6fc85e390f1feec701aeafb2674a8a586b48
Hristo Kolev <hristo.a.kolev@gmail.com>
2018-12-11 23:40:48 +0200

    orm: move to linux part 1

---
+++ src/TrackTv.WebServices/generate-db-classes.sh
@@ -0,0 +1,7 @@
+schema2code \
+-c "Server=vm5.lan;Port=4201;Database=tracktv;Uid=tracktv;Pwd=test123;" \
+-t "./db-classes-template.cshtml" \
+-o "../TrackTv.Data/Poco.cs" \
+-n "TrackTv.Data" \
+-p "TrackTvPocos" \
+-m "TrackTvMetadata"
