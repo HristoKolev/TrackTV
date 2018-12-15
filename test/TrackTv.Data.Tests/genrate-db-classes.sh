fb78afa604d8a9fc960754d5297b8517453bd146
Hristo Kolev <hristo.a.kolev@gmail.com>
2018-12-15 18:55:54 +0200

    orm: test templates

--- test/TrackTv.Data.Tests/genrate-db-classes.sh
+++ test/TrackTv.Data.Tests/genrate-db-classes.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-connectionString="Server=vm5;Port=4202;Database=test;Uid=test;Pwd=test;";
+connectionString="Server=vm5.lan;Port=4202;Database=test;Uid=test;Pwd=test;";
 namespace="TrackTv.Data.Tests";
 pocoClassName="TestDbPocos";
 metadataClassName="TestDbMetadata";
 
 schema2code \
--c  connectionString \
--t "../../src/TrackTv.WebServices/db-classes-template.txt" \
+-c $connectionString \
+-t "../../src/TrackTv.WebServices/db-classes-template.cshtml" \
 -o "./TestPocos.cs" \
--n namespace \
--p pocoClassName \
--m metadataClassName
+-n $namespace \
+-p $pocoClassName \
+-m $metadataClassName
 
 schema2code \
--c connectionString \
--t "./tests-template.txt" \
+-c $connectionString \
+-t "./tests-template.cshtml" \
 -o "./DbTests.cs" \
--n namespace \
--p pocoClassName \
--m metadataClassName
+-n $namespace \
+-p $pocoClassName \
+-m $metadataClassName
