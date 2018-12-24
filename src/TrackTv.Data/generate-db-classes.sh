5e58ebb7c56aeba5154acc88b3f476603e9ff72a
Hristo Kolev <hristo.a.kolev@gmail.com>
2018-12-24 09:59:42 +0200

    project settings

--- src/TrackTv.Data/generate-db-classes.sh
+++ src/TrackTv.Data/generate-db-classes.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 schema2code \
--c "Server=vm5.lan;Port=4201;Database=tracktv;Uid=tracktv;Pwd=test123;" \
+-c "Server=dev-host.lan;Port=4201;Database=tracktv;Uid=tracktv;Pwd=test123;" \
 -t "./db-classes-template.cshtml" \
 -o "./Poco.cs" \
 -n "TrackTv.Data" \
